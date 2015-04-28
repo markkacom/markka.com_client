e18ab706ca0043ae3a800fc7a7a22d025e05e82e
Dirk <incentivetoken@gmail.com>
2015-04-28 15:46:59 +0200

    Transaction namespaced-alias

---
+++ app/plugins/transaction/transactions/namespaced-alias.js
@@ -0,0 +1,56 @@
+(function () {
+'use strict';
+var module = angular.module('fim.base');
+module.run(function (plugins, modals, $q, $rootScope, nxt) {
+  
+  var plugin = plugins.get('transaction');
+
+  plugin.add({
+    label: 'Set Namespaced Alias',
+    id: 'setNamespacedAlias',
+    execute: function (senderRS, args, dont_edit_sender) {
+      args = args||{};
+      return plugin.create(angular.extend(args, {
+        title: 'Set Namespaced Alias',
+        message: 'Set or update a namespaced alias',
+        senderRS: senderRS,
+        editSender: (typeof dont_edit_sender == 'undefined') ? true : !dont_edit_sender,
+        requestType: 'setNamespacedAlias',
+        canHaveRecipient: false,
+        createArguments: function (items) {
+          return {
+            aliasName: items.aliasName,
+            aliasURI: items.aliasURI
+          }
+        },
+        fields: [{
+          label: 'Alias Name',
+          name: 'aliasName',
+          type: 'text',
+          value: args.aliasName||'',
+          validate: function (text) { 
+            this.errorMsg = null;
+            if (!text) { this.errorMsg = null; }
+            else if ( ! plugin.EXTENDED_ALPHABET.test(text)) { this.errorMsg = 'Invalid character'; }
+            else if (plugin.getByteLen(text) > 100) { this.errorMsg = 'To much characters'; }
+            return ! this.errorMsg;
+          },
+          required: true
+        }, {
+          label: 'Alias Value',
+          name: 'aliasURI',
+          type: 'textarea',
+          value: args.aliasURI||'',
+          validate: function (text) { 
+            this.errorMsg = null;
+            if (!text) { this.errorMsg = null; }
+            else if (plugin.getByteLen(text) > 1000) { this.errorMsg = 'To much characters'; }
+            return ! this.errorMsg;
+          },
+          required: true
+        }]
+      }));
+    }
+  });
+});
+})();
