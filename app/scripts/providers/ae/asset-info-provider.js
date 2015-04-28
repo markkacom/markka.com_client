3fec32a925e3f47f621986332769ba7622bb2de2
Dirk <incentivetoken@gmail.com>
2015-04-28 16:11:09 +0200

    Introducing the providers

---
+++ app/scripts/providers/ae/asset-info-provider.js
@@ -0,0 +1,25 @@
+(function () {
+'use strict';
+var module = angular.module('fim.base');
+module.factory('AssetInfoProvider', function (nxt, $q, $timeout) {
+  var cache = {};
+  return {
+    getInfo: function (api, asset) {
+      var c = cache[api.engine.type] || (cache[api.engine.type] = {});
+      var deferred = $q.defer();
+      if (c[asset]) {
+        deferred.resolve(c[asset]);
+      }
+      else {
+        var self = 
+        api.engine.socket().getAsset({ asset: asset }).then(
+          function (data) {
+            deferred.resolve(c[asset] = data);
+          }
+        );
+      }
+      return deferred.promise;
+    }
+  }
+});
+})();
