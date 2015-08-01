39c7a17affdaa951f4801d6e79236c1dbde4a82e
Dirk <incentivetoken@gmail.com>
2015-08-01 14:59:20 +0200

    Multiple updates to master controller

--- app/scripts/controllers/app.js
+++ app/scripts/controllers/app.js
@@ -2,18 +2,43 @@
 'use strict';
 var uriParser = null;
 var module = angular.module('fim.base');
-module.controller('AppController', function($rootScope, $scope, $modal, $q, $log,  
-  $timeout, modals, $window, plugins, serverService, db, settings, $location, 
-  nxt, $route, $translate) {
-
+module.run(function ($rootScope) {
   $rootScope.FIM_SERVER_VERSION = null;
-  $rootScope.TITLE = 'MofoWallet '+VERSION;
+  $rootScope.TITLE = WALLET_NAME+' '+VERSION;
+  $rootScope.WALLET_NAME = WALLET_NAME;
   $rootScope.paramEngine = 'fim';
   $rootScope.enableDualEngines = ENABLE_DUAL_ENGINES;
   $rootScope.isTestnet = IS_TEST_NET;
   $rootScope.forceLocalHost = FORCE_LOCAL_HOST;
+  $rootScope.privateEnabled = PRIVATE_ENABLED;
   $rootScope.multiLanguage = true;
   $rootScope.MONETARY_SYSTEM = false;
+  $rootScope.TRADE_UI_ONLY = TRADE_UI_ONLY;
+});
+module.controller('AppController', function($rootScope, $scope, $modal, $q, $log,  
+  $timeout, modals, $window, plugins, serverService, db, settings, $location, 
+  nxt, $route, $translate, accountsService, BlockchainDownloadProvider) {
+
+  $scope.mainMenuCollapsed=true;
+  $scope.collapseMainMenu = function () {
+    $timeout(function () {
+      $scope.mainMenuCollapsed=true;
+    }, 1000);
+  }
+
+  $rootScope.selectedAccount = null;
+  $rootScope.selectedAccountUnlocked = false;
+  $rootScope.selectableAccounts = [];
+
+  /* Only run when in NodeJS environment */
+  if (isNodeJS) {
+    $scope.fimDownloadProvider = new BlockchainDownloadProvider(nxt.fim(), $scope);
+    $scope.fimDownloadProvider.load();
+    if ($rootScope.enableDualEngines) {
+      $scope.nxtDownloadProvider = new BlockchainDownloadProvider(nxt.nxt(), $scope);
+      $scope.nxtDownloadProvider.load();
+    }
+  }
 
   /* Install app plugins | these all get a menu entry */
   $scope.plugins = [];
@@ -31,78 +56,151 @@ module.controller('AppController', function($rootScope, $scope, $modal, $q, $log
     });
   }
 
+  function getIsSmallDevice() {
+    return window.getComputedStyle(document.getElementById('smallScreenTest')).getPropertyValue('display') == 'block';
+  }
+
+  $rootScope.IS_SMALL_DEVICE = getIsSmallDevice();
+
+  angular.element($window).bind('resize', function () {
+    $rootScope.$evalAsync(function () {
+      $rootScope.IS_SMALL_DEVICE = getIsSmallDevice();
+    })
+  });
+
   $scope.isNodeJS = isNodeJS;
 
   // $rootScope.$on('$translatePartialLoaderStructureChanged', function () {
   //   $translate.refresh();
   // });
 
-  $rootScope.availableLanguages = {
-    'af': 'Afrikaans',
-    'sq': 'Shqip',
-    'ar': 'العربية',
-    'az': 'azərbaycan dili',
-    'eu': 'euskara',
-    'bn': 'বাংলা',
-    'be': 'беларуская мова',
-    'bg': 'български език',
-    'ca': 'català',
-    'zh': '简化中国',
-    'zh-TW': '中國傳統',
-    'hr': 'hrvatski jezik',
-    'cs': 'čeština, český jazyk',
-    'da': 'dansk',
-    'nl': 'Nederlands',
-    'en': 'English',
-    'eo': 'Esperanto',
-    'et': 'eesti',
-    'tl': 'Wikang Filipino',
-    'fi': 'Suomi',
-    'fr': 'Français',
-    'gl': 'Galego',
-    'ka': 'ქართული',
-    'de': 'Deutsch',
-    'el': 'Ελληνικά',
-    'gu': 'ગુજરાતી',
-    'ht': 'Kreyòl Ayisyen',
-    'iw': 'עברית',
-    'hi': 'हिन्दी',
-    'hu': 'Magyar',
-    'is': 'Íslenska',
-    'id': 'Bahasa Indonesia',
-    'ga': 'Gaeilge',
-    'it': 'Italiano',
-    'ja': '日本語',
-    'kn': 'ಕನ್ನಡ',
-    'ko': '조선말',
-    'la': 'Latina',
-    'lv': 'Latviešu',
-    'lt': 'Lietuvių',
-    'mk': 'Mакедонски',
-    'ms': 'Bahasa Melayu',
-    'mt': 'Malti',
-    'no': 'Norsk',
-    'fa': 'فارسی',
-    'pl': 'Polski',
-    'pt': 'Português',
-    'ro': 'Română',
-    'ru': 'Русский',
-    'sr': 'Српски',
-    'sk': 'Slovenčina',
-    'sl': 'Slovenščina',
-    'es': 'Español',
-    'sw': 'Kiswahili',
-    'sv': 'Svenska',
-    'ta': 'தமிழ்',
-    'te': 'తెలుగు',
-    'th': 'ไทย',
-    'tr': 'Türkçe',
-    'uk': 'українська мова',
-    'ur': 'اردو',
-    'vi': 'Việtnam',
-    'cy': 'Cymraeg',
-    'yi': 'ייִדיש'
-  };
+  function loadAccounts() {
+    accountsService.getAll().then(function (accounts) {
+      $scope.$evalAsync(function () {
+        $rootScope.selectableAccounts = accounts;
+        if (!$rootScope.selectedAccount) {
+          var previous_id_rs = window.localStorage.getItem('mofowallet.selected.account');
+          if (previous_id_rs) {
+            for (var i=0;i<accounts.length; i++) {
+              if (accounts[i].id_rs == previous_id_rs) {
+                $rootScope.setSelectedAccount(accounts[i], true);  
+                break;
+              }
+            }
+          }
+          else if ($rootScope.selectableAccounts[0]) {
+            $rootScope.setSelectedAccount($rootScope.selectableAccounts[0], true);  
+          }
+        }
+      });
+    });
+  }
+  accountsService.onChange($rootScope, loadAccounts);
+  loadAccounts();
+
+  $rootScope.setSelectedAccount = function (account, dont_save) {
+    window.localStorage.setItem('mofowallet.selected.account', account.id_rs);
+    $rootScope.$evalAsync(function () {
+      $rootScope.selectedAccount = account;
+      $rootScope.selectedAccountUnlocked = plugins.get('wallet').hasKey(account.id_rs);
+    });
+    var api = nxt.get(account.id_rs);
+    if (api) {
+      api.engine.socket().getAccount({account:account.id_rs}).then(
+        function (data) {
+          $rootScope.$evalAsync(function () {
+            $rootScope.selectedAccount.symbol = api.engine.symbol;
+            $rootScope.selectedAccount.balanceNXT = nxt.util.convertToNXT(data.balanceNQT);
+            $rootScope.selectedAccount.unconfirmedBalanceNXT = nxt.util.convertToNXT(data.unconfirmedBalanceNQT);
+            $rootScope.selectedAccount.name = data.accountName;
+            if (!dont_save) {
+              $rootScope.selectedAccount.save();
+            }
+          });
+        }
+      );
+    }
+  }
+
+  $rootScope.loginAddAccount = function () {
+    modals.open('welcome', {
+      resolve: {
+        items: function () { 
+          return {}; 
+        }
+      }
+    });
+  }
+
+  $rootScope.availableLanguages = [
+    ['en', 'English', 'English', true],
+    ['fi', 'Suomi', 'Finnish', true],
+    ['af', 'Afrikaans', 'Afrikaans'],
+    ['sq', 'Shqip', 'Albanian'],
+    ['ar', 'العربية', 'Arabic'],
+    ['az', 'azərbaycan dili', 'Azerbaijani'],
+    ['eu', 'euskara', 'Basque'],
+    ['bn', 'বাংলা', 'Bengali'],
+    ['be', 'беларуская мова', 'Belarusian'],
+    ['bg', 'български език', 'Bulgarian'],
+    ['ca', 'català', 'Catalan'],
+    ['zh', '简化中国', 'Chinese_Simplified'],
+    ['zh-TW', '中國傳統', 'Chinese_Traditional'],
+    ['hr', 'hrvatski jezik', 'Croatian'],
+    ['cs', 'čeština, český jazyk', 'Czech'],
+    ['da', 'dansk', 'Danish'],
+    ['nl', 'Nederlands', 'Dutch'],
+    ['eo', 'Esperanto', 'Esperanto'],
+    ['et', 'eesti', 'Estonian'],
+    ['tl', 'Wikang Filipino', 'Filipino'],
+    ['fr', 'Français', 'French'],
+    ['gl', 'Galego', 'Galician'],
+    ['ka', 'ქართული', 'Georgian'],
+    ['de', 'Deutsch', 'German'],
+    ['el', 'Ελληνικά', 'Greek'],
+    ['gu', 'ગુજરાતી', 'Gujarati'],
+    ['ht', 'Kreyòl Ayisyen', 'Haitian_Creole'],
+    ['iw', 'עברית', 'Hebrew'],
+    ['hi', 'हिन्दी', 'Hindi'],
+    ['hu', 'Magyar', 'Hungarian'],
+    ['is', 'Íslenska', 'Icelandic'],
+    ['id', 'Bahasa Indonesia', 'Indonesian'],
+    ['ga', 'Gaeilge', 'Irish'],
+    ['it', 'Italiano', 'Italian'],
+    ['ja', '日本語', 'Japanese'],
+    ['kn', 'ಕನ್ನಡ', 'Kannada'],
+    ['ko', '조선말', 'Korean'],
+    ['la', 'Latina', 'Latin'],
+    ['lv', 'Latviešu', 'Latvian'],
+    ['lt', 'Lietuvių', 'Lithuanian'],
+    ['mk', 'Mакедонски', 'Macedonian'],
+    ['ms', 'Bahasa Melayu', 'Malay'],
+    ['mt', 'Malti', 'Maltese'],
+    ['no', 'Norsk', 'Norwegian'],
+    ['fa', 'فارسی', 'Persian'],
+    ['pl', 'Polski', 'Polish'],
+    ['pt', 'Português', 'Portuguese'],
+    ['ro', 'Română', 'Romanian'],
+    ['ru', 'Русский', 'Russian'],
+    ['sr', 'Српски', 'Serbian'],
+    ['sk', 'Slovenčina', 'Slovak'],
+    ['sl', 'Slovenščina', 'Slovenian'],
+    ['es', 'Español', 'Spanish'],
+    ['sw', 'Kiswahili', 'Swahili'],
+    ['sv', 'Svenska', 'Swedish'],
+    ['ta', 'தமிழ்', 'Tamil'],
+    ['te', 'తెలుగు', 'Telugu'],
+    ['th', 'ไทย', 'Thai'],
+    ['tr', 'Türkçe', 'Turkish'],
+    ['uk', 'українська мова', 'Ukrainian'],
+    ['ur', 'اردو', 'Urdu'],
+    ['vi', 'Việtnam', 'Vietnamese'],
+    ['cy', 'Cymraeg', 'Welsh'],
+    ['yi', 'ייִדיש', 'Yiddish']
+  ];
+  $rootScope.availableLanguages.forEach(function (entry) {
+    $rootScope.availableLanguages[entry[0]] = entry[1];
+  });
 
   $rootScope.langCode = $translate.preferredLanguage();
   $rootScope.langName = $rootScope.availableLanguages[$rootScope.langCode];
@@ -114,6 +212,16 @@ module.controller('AppController', function($rootScope, $scope, $modal, $q, $log
 
   if (!$rootScope.multiLanguage) {
     $rootScope.setLang('en');
+  }
+
+  $scope.showLanguageModal = function () {
+    modals.open('language', {
+      resolve: {
+        items: function () { return {}; }
+      },
+      close: function () {
+      }
+    });    
   }
 
   $scope.reloadMofoWallet = function () {
