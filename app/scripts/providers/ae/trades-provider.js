4362991a6b732b3cb0511d217f48755cde855109
Dirk <incentivetoken@gmail.com>
2015-08-01 15:47:39 +0200

    Updating Trades provider

--- app/scripts/providers/ae/trades-provider.js
+++ app/scripts/providers/ae/trades-provider.js
@@ -3,11 +3,17 @@
 var module = angular.module('fim.base');
 module.factory('TradesProvider', function (nxt, $q, IndexedEntityProvider) {
   
-  function TradesProvider(api, $scope, pageSize, asset) {
+  function TradesProvider(api, $scope, pageSize, asset, decimals) {
     this.init(api, $scope, pageSize);
     this.asset = asset;
+    this.decimals = decimals;
 
-    //api.engine.socket().subscribe('addedTrades*'+asset, angular.bind(this, this.addedTrades), $scope);
+    api.engine.socket().subscribe('blockPopped', angular.bind(this, this.blockPopped), $scope);
+    api.engine.socket().subscribe('blockPushed', angular.bind(this, this.blockPushed), $scope);
+
+    api.engine.socket().subscribe('TRADE_ADDED*'+asset, angular.bind(this, this.tradeAddUpdate), $scope);
+    api.engine.socket().subscribe('TRADE_UPDATED*'+asset, angular.bind(this, this.tradeAddUpdate), $scope);
+    api.engine.socket().subscribe('TRADE_REMOVED*'+asset, angular.bind(this, this.tradeRemove), $scope); 
   }
   angular.extend(TradesProvider.prototype, IndexedEntityProvider.prototype, {
 
@@ -15,27 +21,36 @@ module.factory('TradesProvider', function (nxt, $q, IndexedEntityProvider) {
     sortFunction: IndexedEntityProvider.prototype.transactionSort,
 
     /* @override */
-    uniqueKey: function (trade) { return trade.id; },
+    uniqueKey: function (trade) { return trade.bidOrder+trade.askOrder; },
 
     getData: function (firstIndex) {
       var deferred = $q.defer();
       var args = {
         asset:          this.asset,
         firstIndex:     firstIndex,
-        lastIndex:      firstIndex + this.pageSize
-      }
-      this.api.engine.socket().getAssetTrades(args).then(deferred.resolve, deferred.reject);
+        lastIndex:      firstIndex + this.pageSize,
+        requestType:    'getVirtualTrades'
+      }      
+      this.api.engine.socket().callAPIFunction(args).then(deferred.resolve, deferred.reject);
       return deferred.promise;
     },
 
+    translate: function (trade) {
+      if (trade.quantityQNT) {
+        trade.quantity = nxt.util.convertToQNTf(trade.quantityQNT, this.decimals);
+      }
+      if (trade.priceNQT) {
+        trade.price    = nxt.util.calculateOrderPricePerWholeQNT(trade.priceNQT, this.decimals);
+        trade.total    = nxt.util.convertToNXT(nxt.util.calculateOrderTotalNQT(trade.priceNQT, trade.quantityQNT));
+      }
+      if (trade.timestamp) {
+        trade.date     = nxt.util.formatTimestamp(trade.timestamp, true);
+      }
+    },
+
     dataIterator: function (data) {
-      var decimals = data.decimals;
       for (var i=0; i<data.trades.length; i++) {
-        var a      = data.trades[i];
-        a.quantity = nxt.util.convertToQNTf(a.quantityQNT, decimals);
-        a.price    = nxt.util.calculateOrderPricePerWholeQNT(a.priceNQT, decimals);
-        a.total    = nxt.util.convertToNXT(nxt.util.calculateOrderTotalNQT(a.priceNQT, a.quantityQNT));
-        a.date     = nxt.util.formatTimestamp(a.timestamp, true);
+        this.translate(data.trades[i]);
       }
       return new Iterator(data.trades);
     },
@@ -44,8 +59,40 @@ module.factory('TradesProvider', function (nxt, $q, IndexedEntityProvider) {
       return type == 'buy' ? 'green' : 'red';
     },
 
-    calculate24HChange: function () {
+    /* @websocket */
+    tradeAddUpdate: function (trade) {
+      var self = this;
+      this.$scope.$evalAsync(function () {
+        self.translate(trade);
+        self.add(trade);
+      });
+    },
 
+    /* @websocket */
+    tradeRemove: function (trade) {
+      var self = this;
+      this.$scope.$evalAsync(function () {
+        self.remove(self.uniqueKey(trade));
+      });
+    },
+
+    /* @websocket */
+    blockPushed: function (block) {
+      var self = this;
+      this.$scope.$evalAsync(function () {
+        self.forEach(function (trade) {
+          trade.confirmations = block.height - trade.height;
+        });
+      });
+    },
+
+    /* @websocket */
+    blockPopped: function (block) {
+      if (this.delayedReload) {
+        clearTimeout(this.delayedReload);
+      }
+      var self = this;
+      this.delayedReload = setTimeout(function () { self.reload(); }, 1000);
     }
   });
   return TradesProvider;
