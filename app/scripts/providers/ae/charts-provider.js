afb036f17cfc285f806027a29816df9149fbf024
Dirk <incentivetoken@gmail.com>
2015-08-01 15:19:37 +0200

    Updating Charts provider

--- app/scripts/providers/ae/charts-provider.js
+++ app/scripts/providers/ae/charts-provider.js
@@ -32,13 +32,20 @@ module.factory('ChartsProvider', function (nxt, $q, $timeout) {
   var DAY = 2;
   var WEEK = 3;
 
-  function ChartsProvider(api, asset, $scope) {
+  var ONE_DAY_MS = 1000*60*60*24;
+  var ONE_WEEK_MS = ONE_DAY_MS*7;
+
+  function ChartsProvider(api, $scope, asset, decimals) {
     this.api       = api;
     this.asset     = asset;
+    this.decimals  = decimals;
     this.$scope    = $scope;
     this.isLoading = true;
     this.chartdata = [];
     this.chart     = null;
+
+    api.engine.socket().subscribe('blockPopped', angular.bind(this, this.blockPopped), $scope);
+    api.engine.socket().subscribe('VIRTUAL_TRADE*'+asset, angular.bind(this, this.virtualTrade), $scope);
   }
   ChartsProvider.prototype = {
     reload: function () {
@@ -51,22 +58,27 @@ module.factory('ChartsProvider', function (nxt, $q, $timeout) {
       });
     },
 
+    translate: function (d) {
+      d.date  = nxt.util.timestampToDate(d.timestamp);
+      d.open  = parseFloat(nxt.util.calculateOrderPricePerWholeQNT(d.open, this.decimals));
+      d.close = parseFloat(nxt.util.calculateOrderPricePerWholeQNT(d.close, this.decimals));
+      d.high  = parseFloat(nxt.util.calculateOrderPricePerWholeQNT(d.high, this.decimals));
+      d.low    = parseFloat(nxt.util.calculateOrderPricePerWholeQNT(d.low, this.decimals));
+      d.volume = parseFloat(nxt.util.convertToQNTf(d.vol, this.decimals));
+      d.value  = d.open;
+    },
+
     getNetworkData: function () {
       var self = this;
       this.api.engine.socket().getAssetChartData({ asset: this.asset, window: HOUR }).then(
         function (data) {
           self.$scope.$evalAsync(function () {
             self.isLoading = false;
-            var decimals = data.asset.decimals;
+            if (!data.asset) return;
+            var d;
             for (var i=0, d; i<data.data.length; i++) {
-              d       = data.data[i];
-              d.date  = nxt.util.timestampToDate(d.timestamp);
-              d.open  = parseFloat(nxt.util.calculateOrderPricePerWholeQNT(d.open, decimals));
-              d.close = parseFloat(nxt.util.calculateOrderPricePerWholeQNT(d.close, decimals));
-              d.high  = parseFloat(nxt.util.calculateOrderPricePerWholeQNT(d.high, decimals));
-              d.low    = parseFloat(nxt.util.calculateOrderPricePerWholeQNT(d.low, decimals));
-              d.volume = parseFloat(nxt.util.convertToQNTf(d.vol, decimals));
-              d.value  = d.open;
+              d = data.data[i];
+              self.translate(d);
               self.chartdata.push(d);
             }
 
@@ -75,19 +87,13 @@ module.factory('ChartsProvider', function (nxt, $q, $timeout) {
             if (self.chart == null) {
               self.buildChart().then(function (chart) {
                 self.chart = chart;
-                $timeout(function () {
-                  self.chart.invalidateSize();
-                  self.chart.zoomOut();
-                }, 50, false);
+                self.layout();
               });              
             }
             else {
               self.chart.dataSets[0].dataProvider = self.chartdata;
               self.chart.validateData();
-              $timeout(function () {
-                self.chart.invalidateSize();
-                self.chart.zoomOut();
-              }, 50, false);              
+              self.layout();              
             }
             //console.log('chartdata', data);
           });
@@ -100,6 +106,52 @@ module.factory('ChartsProvider', function (nxt, $q, $timeout) {
       );
     },
 
+    /* @websocket */
+    blockPopped: function (block) {
+      if (this.delayedReload) {
+        clearTimeout(this.delayedReload);
+      }
+      var self = this;
+      this.delayedReload = setTimeout(function () { self.reload(); }, 1000);
+    },
+
+    /* @websocket */
+    virtualTrade: function (trade) {
+      this.blockPopped();
+      // for (var i=0; i<this.chartdata.length; i++) {
+      //   if (this.chartdata[i].timestamp == trade.timestamp) {
+      //     return;
+      //   }
+      // }
+      // var d = {
+      //   open: trade.priceNQT,
+      //   vol: trade.quantityQNT,
+      //   date: nxt.util.timestampToDate(trade.timestamp)
+      // };
+      // this.translate(d);
+      // this.chartdata.push(d);
+      // this.chartdata.sort(function (a, b) { return a.timestamp - b.timestamp });
+      // if (this.delayedLayout) {
+      //   clearTimeout(this.delayedLayout);
+      // }
+      // var self = this;
+      // this.delayedLayout = setTimeout(function () {
+      //   self.chart.validateData();
+      //   self.chart.invalidateSize();
+      //   self.chart.zoomOut();
+      // }, 50);
+    },
+
+    layout: function () {
+      var self = this;
+      $timeout(function () {
+        self.chart.invalidateSize();
+        self.chart.periodSelector.setDefaultPeriod();
+        //self.chart.zoomOut();
+        //self.chart.zoom(new Date(Date.now() - ONE_DAY_MS), new Date(Date.now()));
+      }, 50, false);
+    },
+
     buildChart: function () {
       var deferred = $q.defer();
       var self = this;
@@ -107,21 +159,24 @@ module.factory('ChartsProvider', function (nxt, $q, $timeout) {
         var colors = getBootstrapColors();
         $timeout(function () {
           deferred.resolve(self._buildChart(colors));
-        });
+        }, 200);
       }, 1, false);
       return deferred.promise;
     },
 
     _buildChart: function (colors) {
-      return AmCharts.makeChart("chartdiv", {
+      var stockChart = AmCharts.makeChart("chartdiv", {
         type: "stock",
         theme: "none",
         pathToImages: "/images/",
           
         categoryAxesSettings: {
-          minPeriod: "mm",
-          equalSpacing: true
+          minPeriod: "15ss",
+          equalSpacing: true,
+          maxSeries: 1000
         },
+
+        extendToFullPeriod: false,
 
         dataSets: [{
           lineColor: colors.lineColor,
@@ -194,27 +249,30 @@ module.factory('ChartsProvider', function (nxt, $q, $timeout) {
         },
 
         periodSelector: {
-          position: "top",
-          dateFormat: "YYYY-MM-DD JJ:NN",
-          inputFieldWidth: 150,
+          position: "bottom",
+          dateFormat: "YYYY-MM-DD",
+          inputFieldWidth: 50,
           inputFieldsEnabled:false,
           periods: [{
             period: "hh",
-            count: 1,
             label: "1 hour",
+            count: 1,
             selected: true
           }, {
             period: "DD",
+            label: "1 day",
             count: 1,
-            label: "1 day"
+            selected: false
           }, {
             period: "DD",
+            label: "1 week",
             count: 7,
-            label: "1 week"
+            selected: false
           }, {
             period: "MM",
             count: 1,
-            label: "1 month"
+            label: "1 month",
+            selected: false
           }, {
             period: "MAX",
             label: "MAX"
@@ -225,6 +283,7 @@ module.factory('ChartsProvider', function (nxt, $q, $timeout) {
           usePrefixes: true
         }
       });
+      return stockChart;
     }
   }
   return ChartsProvider
