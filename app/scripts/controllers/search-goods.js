430f9cb223b70eaee845760d130fb829a08105cc
Ali Asgher <aliasgherlakkadshaw@gmail.com>
2015-08-12 15:00:13 +0530

    filter by tags in search goods

--- app/scripts/controllers/search-goods.js
+++ app/scripts/controllers/search-goods.js
@@ -42,27 +42,27 @@ module.controller('SearchGoodsCtrl', function($location, $q, $scope, modals, $ro
 						$scope.tags.push(eachTags);
 					})
 				})
-						// var unique = function(origArr) {
-						//     var newArr = [],
-						//         origLen = origArr.length,
-						//         found, x, y;
+						var unique = function(origArr) {
+						    var newArr = [],
+						        origLen = origArr.length,
+						        found, x, y;
 
-						//     for (x = 0; x < origLen; x++) {
-						//         found = undefined;
-						//         for (y = 0; y < newArr.length; y++) {
-						//             if (origArr[x] === newArr[y]) {
-						//                 found = true;
-						//                 break;
-						//             }
-						//         }
-						//         if (!found) {
-						//             newArr.push(origArr[x]);
-						//         }
-						//     }
-						//     return newArr;
-						// }
+						    for (x = 0; x < origLen; x++) {
+						        found = undefined;
+						        for (y = 0; y < newArr.length; y++) {
+						            if (origArr[x] === newArr[y]) {
+						                found = true;
+						                break;
+						            }
+						        }
+						        if (!found) {
+						            newArr.push(origArr[x]);
+						        }
+						    }
+						    return newArr;
+						}
 
-						// $scope.uniqueTags = unique($scope.tags);
+						$scope.uniqueTags = unique($scope.tags);
 			})
 		}
 
