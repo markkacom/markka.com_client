f7c8324fced47ce66ca4d6174f0f26ec81d45738
Ali Asgher <aliasgherlakkadshaw@gmail.com>
2015-08-18 14:52:49 +0530

    indentation fixed

--- app/scripts/controllers/past-orders.js
+++ app/scripts/controllers/past-orders.js
@@ -1,24 +1,24 @@
-(function () {
-'use strict';
-var module = angular.module('fim.base');
+(function() {
+  'use strict';
+  var module = angular.module('fim.base');
 
-module.controller('PastOrdersCtrl', function($scope, $routeParams, nxt, shoppingCartService) {
+  module.controller('PastOrdersCtrl', function($scope, $routeParams, nxt, shoppingCartService) {
 
-		$scope.id_rs          = $routeParams.id_rs;
+    $scope.id_rs = $routeParams.id_rs;
 
-		var api = nxt.get($scope.id_rs);
+    var api = nxt.get($scope.id_rs);
 
-		$scope.shoppingCart = shoppingCartService.get();
+    $scope.shoppingCart = shoppingCartService.get();
 
-		var past_orders_args = {
-			requestType: 'getDGSPurchases',
-			seller: $scope.id_rs
-		}
+    var past_orders_args = {
+      requestType: 'getDGSPurchases',
+      seller: $scope.id_rs
+    }
 
-		api.engine.socket().callAPIFunction(past_orders_args).then(function(data) {
-			$scope.pastOrders = data;
-			console.log(data);
-		})
+    api.engine.socket().callAPIFunction(past_orders_args).then(function(data) {
+      $scope.pastOrders = data;
+      console.log(data);
+    })
 
-	})
+  })
 })();
