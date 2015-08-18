08fcd9cbbaddae114e9ff4f266a8a2a8e61b2ea2
Ali Asgher <aliasgherlakkadshaw@gmail.com>
2015-08-18 20:02:25 +0530

    cart view and cart controller are not separate now

--- app/scripts/controllers/goods.js
+++ app/scripts/controllers/goods.js
@@ -43,6 +43,50 @@
       console.log('listing');
     }
 
+    else if($scope.paramSection == 'cart') {
+      $scope.shoppingCart = shoppingCartService.get();
+      console.log($scope.shoppingCart);
+
+      $scope.shoppingCart.forEach(function(good) {
+        try {
+          var data = JSON.parse(good.description);
+        } catch (ex) {
+          console.log("Unable to parse");
+        }
+        if (data) {
+          good.description = data.description;
+          good.image = data.image;
+          good.callback = data.callback;
+        }
+      });
+
+      $scope.placeOrder = function() {
+        processCart($scope.shoppingCart);
+      }
+
+      function processCart(shoppingCart) {
+        if (shoppingCart.length > 0) {
+          var shoppingCartGoods = shoppingCart[0];
+          var order_args = {
+            requestType: "dgsPurchase",
+            goods: shoppingCartGoods.goods,
+            priceNQT: shoppingCartGoods.priceNQT,
+            deliveryDeadlineTimestamp: String(nxt.util.convertToEpochTimestamp(Date.now()) + 60 * 60 * 168)
+          }
+          plugins.get('transaction').get('dgsPurchase').execute($scope.id_rs, order_args).then(function() {
+            shoppingCartService.removeItem(0);
+            shoppingCart.splice(0, 1);
+            processCart(shoppingCart);
+          });
+        }
+      }
+
+      $scope.remove = function(index) {
+        var abc = shoppingCartService.removeItem(index);
+        $scope.shoppingCart.splice(index, 1);
+      }
+    }
+
     else {
       console.log('details');
       var details_args = {
@@ -59,7 +103,7 @@
       $scope.addToCart = function(goodsDetails) {
         var cartDetails = shoppingCartService.add(goodsDetails);
         console.log(cartDetails);
-        $location.path('/goods/' + $scope.id_rs + '/goods/viewcart');
+        $location.path('/goods/' + $scope.id_rs + '/cart');
       }
     }
   });
