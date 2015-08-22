1f9c28a58f76fc8d2ee9bc6857541c75a3a05a6e
Ali Asgher <aliasgherlakkadshaw@gmail.com>
2015-08-22 15:11:28 +0530

    a.index value changed

--- app/scripts/providers/goods-provider.js
+++ app/scripts/providers/goods-provider.js
@@ -39,9 +39,10 @@
 
       dataIterator: function(data) {
         var goods = data.goods;
-        var index = index;
+        var index = this.entities.length > 0 ? this.entities[this.entities.length - 1].index : 0;
         for (var i = 0; i < goods.length; i++) {
           var a = goods[i];
+          a.index = index;
         }
         return new Iterator(goods);
       }
