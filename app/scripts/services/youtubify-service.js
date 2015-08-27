404a9e4f0a263b8b87223c02e34833e820ab6acd
Dirk <incentivetoken@gmail.com>
2015-08-27 21:35:37 +0200

    Add (to be) youtube service

---
+++ app/scripts/services/youtubify-service.js
@@ -0,0 +1,14 @@
+(function () {
+'use strict';
+var module = angular.module('fim.base');
+module.factory('Youtubify', function () {
+
+  /* TODO - similar to Emoji support - dropping in youtube urls should translate 
+     to youtube embed displayed in the chat balloons */
+
+  var SERVICE = {
+
+  };
+  return SERVICE;
+});
+})();
