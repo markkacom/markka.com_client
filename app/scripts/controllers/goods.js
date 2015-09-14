a7cae16a970f3e6338239f0b6ec5f1efb5c3fc12
Ali Asgher <aliasgherlakkadshaw@gmail.com>
2015-09-14 18:37:48 +0530

    misc

--- app/scripts/controllers/goods.js
+++ app/scripts/controllers/goods.js
@@ -121,6 +121,7 @@
 
       $scope.pastGoods = new PastGoodsProvider(api, $scope, 10, $scope.id_rs);
       $scope.pastGoods.reload();
+      console.log($scope.pastGoods);
       $scope.msg = function(id) {
         var recipient_args = {
           recipient: id
