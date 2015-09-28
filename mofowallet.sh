da25264eae5260158cebba0d725f5f36c7e8a11d
Dirk <incentivetoken@gmail.com>
2015-09-28 10:14:00 +0200

    Add dice words to the build scripts

--- mofowallet.sh
+++ mofowallet.sh
@@ -34,7 +34,9 @@ echo "successfully built mofowallet"
 
 # copy over all code to git/mofo
 echo "copy mofo files to jekyll repo"
-MOFO_FILES="dist/fonts dist/images dist/partials dist/plugins dist/scripts dist/styles dist/favicon.ico dist/i18n dist/amstockchart dist/mode.js"
+MOFO_FILES="dist/fonts dist/images dist/partials \
+  dist/plugins dist/scripts dist/styles dist/favicon.ico \
+  dist/i18n dist/amstockchart dist/mode.js dist/dice-words"
 cp -r -p $MOFO_FILES ~/git/mofo
 cp dist/index.html ~/git/mofo/launch.html
 DATE=`date`
