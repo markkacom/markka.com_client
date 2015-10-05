cd146c685527366063485b22a460aaa487bd9b22
Dirk <incentivetoken@gmail.com>
2015-10-05 13:35:16 +0200

    Completing review of goods controller

--- app/scripts/controllers/goods.js
+++ app/scripts/controllers/goods.js
@@ -1,178 +1,231 @@
 (function() {
-  var module = angular.module('fim.base');
-  module.config(function($routeProvider) {
-    $routeProvider
-      .when('/goods/:id_rs/:listing', {
-        templateUrl: 'partials/goods.html',
-        controller: 'GoodsCtrl'
-      })
+var module = angular.module('fim.base');
+module.config(function($routeProvider) {
+  $routeProvider
+    .when('/goods/:engine/:section/:id_rs?', {
+      templateUrl: 'partials/goods.html',
+      controller: 'GoodsCtrl'
+    })
+});
+
+module.controller('GoodsCtrl', function($location, $rootScope, $scope, $http, $routeParams, nxt, plugins, 
+  shoppingCartService, AllGoodsProvider, PastGoodsProvider, GoodsDetailsProvider, UserGoodsProvider, 
+  SoldGoodsProvider, DeliveryConfirmedGoodsProvider, Gossip, db) {
+
+  $scope.paramEngine  = $rootScope.paramEngine = $routeParams.engine;
+  $scope.paramSection = $routeParams.section;  
+  $scope.id_rs        = $routeParams.id_rs;
+
+  var api             = $scope.paramEngine == 'fim' ? nxt.fim() : nxt.nxt();
+  $scope.symbol       = api.engine.symbol;
+
+  /* @param items Array of CartModel (@see models/cart.js) */
+  function setupShoppingCart(items) {
+    $scope.$evalAsync(function () {
+      $scope.shoppingCart = items||[];
+      $scope.shoppingCart.forEach(calculateItemTotal);
+      calculateCartTotal();
+    });
+  }
+
+  shoppingCartService.getAll(api.engine.symbol).then(setupShoppingCart);
+  db.cart.addObserver($scope, { 
+    finally: function () {
+      shoppingCartService.getAll(api.engine.symbol).then(setupShoppingCart);
+    }
   });
 
-  module.controller('GoodsCtrl', function($location, $rootScope, $scope, $http, $routeParams, nxt, plugins, shoppingCartService, AllGoodsProvider, PastGoodsProvider, GoodsDetailsProvider, UserGoodsProvider, SoldGoodsProvider, DeliveryConfirmedGoodsProvider, Gossip) {
-
-    $scope.id_rs = $routeParams.id_rs;
-    $scope.paramSection = $routeParams.listing;
-
-    var api = nxt.get($scope.id_rs);
-    $scope.symbol= api.engine.symbol
-
-    $scope.shoppingCart = shoppingCartService.get();
-
-    $scope.deleteGood = function(good, index) {
-      var deleteGoodArgs = {
-        // requestType: 'dgsDelisting',
-        goods: good.goods
+  switch ($scope.paramSection) {
+    /* /goods/nxt/listing OR /goods/nxt/listing/NXT-7JDS-CS72-TNGS-BBE8E */
+    case 'listing': {
+      $scope.provider = new AllGoodsProvider(api, $scope, 10, $scope.id_rs);
+      $scope.provider.reload();
+      break;
+    }
+    /* /goods/nxt/tags/some-tag */
+    case 'tags': {
+      $scope.id_rs = null;
+      $scope.provider = new AllGoodsProvider(api, $scope, 10, null, $routeParams.id_rs); /* we use id_rs for the tag parameter */
+      $scope.provider.reload();
+      break;
+    }    
+    /* /goods/nxt/pastorders/NXT-7JDS-CS72-TNGS-BBE8E */
+    case 'pastorders': {
+      if ($scope.id_rs) {
+        $scope.provider = new PastGoodsProvider(api, $scope, 10, $scope.id_rs);
+        $scope.provider.reload();
       }
-      plugins.get('transaction').get('dgsDelisting').execute($scope.id_rs, deleteGoodArgs)        
+      else {
+        $location.path('/goods/'+$scope.paramEngine+'/listing');
+      }
+      break;
     }
+    /* /goods/nxt/solditems/NXT-7JDS-CS72-TNGS-BBE8E */
+    case 'solditems': {
+      if ($scope.id_rs) {
+        // for pending
+        $scope.soldGoods = new SoldGoodsProvider(api, $scope, 10, $scope.id_rs);
+        $scope.soldGoods.reload();
 
-    $scope.details = function(goodsDetails) {
-      $location.path('/goods/' + $scope.id_rs + '/' + goodsDetails.goods);
+        // for Completed
+        $scope.deliveryConfirmedGoods = new DeliveryConfirmedGoodsProvider(api, $scope, $scope.id_rs);
+        $scope.deliveryConfirmedGoods.reload();
+      }
+      else {
+        $location.path('/goods/'+$scope.paramEngine+'/listing');
+      }
+      break;
     }
-
-    $scope.add = function() {
-      // var args = {
-      //   requestType: 'dgsListing'
-      // }
-      plugins.get('transaction').get('dgsListing').execute();
+    /* /goods/nxt/cart OR /goods/nxt/cart/NXT-7JDS-CS72-TNGS-BBE8E */
+    case 'cart': {
+      /* nothing to do here $scope.shoppingCart is setup elsewhere */
+      break;      
     }
+    /* /goods/nxt/142563763572767 */
+    default: {
+      $scope.provider = new GoodsDetailsProvider(api, $scope, $scope.paramSection);
+      $scope.provider.reload();
+      $scope.paramSection = 'detail';      
+    }
+  }
 
-    if ($scope.paramSection == 'listing') {
-      $scope.showGoods = new AllGoodsProvider(api, $scope, 10, $scope.id_rs);
-      $scope.showGoods.reload();
-    } else if ($scope.paramSection == 'cart') {
-      $scope.shoppingCart = shoppingCartService.get();
-      $scope.total = 0;
-      $scope.shoppingCart.forEach(function(good) {
-        $scope.total += parseFloat(good.priceNQT)
-        try {
-          var data = JSON.parse(good.description);
-        } catch (ex) {
-          console.log("Unable to parse");
-        }
-        if (data) {
-          good.description = data.description;
-          good.image = data.image;
-          good.callback = data.callback;
-        }
-      });
-      $scope.placeOrder = function() {
-        $scope.balance = $rootScope.userData.balanceNXT;
-          if($scope.total >= $scope.balance){
-            $scope.balanceError = "You don't have enough balance to place these orders.";
-          } else {
-            processCart($scope.shoppingCart);
-            $scope.balanceError = ' ';
+  /* @param item CartModel */
+  $scope.quantityChanged = function (item) {
+    $scope.$evalAsync(function () {
+      calculateItemTotal(item);
+      calculateCartTotal();
+      item.save();
+    });
+  }
+
+  function calculateCartTotal() {
+    var totalNQT = '0';
+    $scope.shoppingCart.forEach(function (item) { 
+      totalNQT = (new BigInteger(totalNQT)).add(
+                    new BigInteger(nxt.util.convertToNQT(item.totalNXT))).toString() 
+    });
+    $scope.totalNXT = nxt.util.convertToNXT(totalNQT.toString());
+  }
+
+  /* @param item CartModel */
+  function calculateItemTotal(item) {
+    item.totalNXT = nxt.util.convertToNXT((new BigInteger(item.priceNQT)).multiply(new BigInteger(""+item.count)).toString());
+  }
+
+  $scope.msg = function(id) {
+    plugins.get('transaction').get('sendMessage').execute($scope.id_rs, { recipient: id, editRecipient: false });
+  }
+
+  $scope.deleteGood = function(good, index) {
+    plugins.get('transaction').get('dgsDelisting').execute({goods: good.goods});
+  }
+
+  $scope.details = function(good) {
+    $location.path('/goods/' + $scope.paramEngine + '/' + good.goods);
+  }
+
+  $scope.add = function() {
+    plugins.get('transaction').get('dgsListing').execute();
+  }
+
+  $scope.placeOrder = function() {
+    var result = (new BigInteger(nxt.util.convertToNQT($scope.totalNXT))).compareTo(
+                    new BigInteger(nxt.util.convertToNQT($rootScope.userData.balanceNXT)));
+    if (result <= 0) {
+      $scope.balanceError = "You don't have enough balance to place these orders.";
+    }
+    else {
+      $scope.balanceError = ' ';
+      var iterator = new Iterator($scope.shoppingCart);
+      processCart(iterator);
+    }
+  }
+
+  function processCart(iterator) {
+    if (iterator.hasMore()) {
+      var item = iterator.next();
+      var order_args = {
+        goods: item.goods,
+        priceNXT: nxt.util.convertToNXT(item.priceNQT),
+        quantity: item.count,
+        name: item.name,
+        deliveryDeadlineTimestamp: String(nxt.util.convertToEpochTimestamp(Date.now()) + 60 * 60 * 168),
+        recipient: item.sellerRS
+      }
+      plugins.get('transaction').get('dgsPurchase').execute(order_args).then(
+        function(data) {
+          if(data) { 
+            // $http({
+            //   url: item.callback,
+            //   data: item,
+            //   method: 'POST'
+            // }).success(function(data) {
+            //   console.log(data);
+            // }).error(function(err) {
+            //   console.log(err);
+            // });
+            item.delete().then(
+              function () {
+                if (iterator.hasMore()) {
+                  processCart(iterator);
+                }
+                else {
+                  $scope.$evalAsync(function () {
+                    $scope.successful = "Payment Completed";
+                  });
+                }
+              }              
+            );
           }
-      }
-
-      function processCart(shoppingCart) {
-        if (shoppingCart.length > 0) {
-          var shoppingCartGoods = shoppingCart[0];
-          console.log(shoppingCartGoods);
-          var order_args = {
-            // requestType: "dgsPurchase",
-            goods: shoppingCartGoods.goods,
-            priceNQT: shoppingCartGoods.priceNQT,
-            name: shoppingCartGoods.name,
-            deliveryDeadlineTimestamp: String(nxt.util.convertToEpochTimestamp(Date.now()) + 60 * 60 * 168),
-            recipient: shoppingCartGoods.seller
-          }
-          plugins.get('transaction').get('dgsPurchase').execute($scope.id_rs, order_args).then(function(data) {
-            if(data) { 
-                $http({
-                  url: shoppingCartGoods.callback,
-                  data: shoppingCartGoods,
-                  method: 'POST'
-                }).success(function(data) {
-                  console.log(data);
-                }).error(function(err) {
-                  console.log(err);
-                })
-                shoppingCartService.removeItem(0);
-                shoppingCart.splice(0, 1);
-                processCart(shoppingCart);
-
-                $scope.successful = "Payment Completed";
-            }
-          });
         }
-      }
-
-      $scope.remove = function(index, items) {
-        var abc = shoppingCartService.removeItem(index);
-        $scope.shoppingCart.splice(index, 1);
-        $scope.total -= parseFloat(items.priceNQT);
-      }
-    } else if ($scope.paramSection == "pastorders") {
-
-      $scope.shoppingCart = shoppingCartService.get();
-
-      $scope.pastGoods = new PastGoodsProvider(api, $scope, 10, $scope.id_rs);
-      $scope.pastGoods.reload();
-      console.log($scope.pastGoods);
-      $scope.msg = function(id) {
-        var recipient_args = {
-          recipient: id
-        }
-        plugins.get('transaction').get('sendMessage').execute($scope.id_rs, recipient_args);
-      }
-    } else if ($scope.paramSection == "viewlisting") {
-      $scope.showGoods = new UserGoodsProvider(api, $scope, 10, $scope.id_rs);
-      $scope.showGoods.reload();
-    } else if ($scope.paramSection == 'solditems') {
-      // for pending
-      $scope.soldGoods = new SoldGoodsProvider(api, $scope, 10, $scope.id_rs);
-      $scope.soldGoods.reload();
-
-      // for Completed
-      $scope.deliveryConfirmedGoods = new DeliveryConfirmedGoodsProvider(api, $scope, $scope.id_rs);
-      $scope.deliveryConfirmedGoods.reload();
-
-      $scope.decrypt = function(encryptedMessage, index) {
-        if(encryptedMessage) {
-          var publicKey = api.crypto.secretPhraseToPublicKey($rootScope.currentAccount.secretPhrase);
-          $scope.textMessage = index;
-          $scope.decryptedMessage = Gossip.decryptMessage(publicKey, encryptedMessage.nonce, encryptedMessage.data)
-        }
-      }
-      $scope.rebate = function(rebateOrder) {
-        var rebate_args = {
-          // requestType: "dgsRefund",
-          purchase: rebateOrder.purchase,
-          refundNQT: rebateOrder.priceNQT
-        }
-        plugins.get('transaction').get('dgsRefund').execute($scope.id_rs, rebate_args).then(function(data) {
-        })
-      }
-      $scope.confirmDelivery = function(deliveryItem) {
-        var confirmDelivery_args = {
-          // requestType: "dgsDelivery",
-          purchase: deliveryItem.purchase
-        }
-        plugins.get('transaction').get('dgsDelivery').execute($scope.id_rs, confirmDelivery_args).then(function(data) {
-        })
-      }
-    } else {
-
-      $scope.goodsDetails = new GoodsDetailsProvider(api, $scope, $scope.paramSection);
-      $scope.goodsDetails.reload();
-      $scope.good = $scope.goodsDetails.entities;
-
-      $scope.addToCart = function(goodsDetails) {
-        var cartDetails = shoppingCartService.add(goodsDetails);
-        $location.path('/goods/' + $scope.id_rs + '/cart');
-      }
+      );
     }
-  }).filter('parseImage', function() {
-    return function(item) {
-      var data = JSON.parse(item);
-      return data.image;
+  }
+
+  $scope.decrypt = function(encryptedMessage, index) {
+    if(encryptedMessage) {
+      var publicKey = api.crypto.secretPhraseToPublicKey($rootScope.currentAccount.secretPhrase);
+      $scope.textMessage = index;
+      $scope.decryptedMessage = Gossip.decryptMessage(publicKey, encryptedMessage.nonce, encryptedMessage.data)
     }
-  }).filter('parseDescription', function() {
-    return function(item) {
-      var data = JSON.parse(item);
-      return data.description;
+  }
+
+  $scope.rebate = function(rebateOrder) {
+    var rebate_args = {
+      purchase: rebateOrder.purchase,
+      refundNQT: rebateOrder.priceNQT
     }
-  })
+    plugins.get('transaction').get('dgsRefund').execute(rebate_args);
+  }
+
+  $scope.confirmDelivery = function(deliveryItem) {
+    var confirmDelivery_args = {
+      purchase: deliveryItem.purchase
+    }
+    plugins.get('transaction').get('dgsDelivery').execute(confirmDelivery_args);
+  }
+
+  $scope.addToCart = function(details) {
+    shoppingCartService.add($scope.symbol, details, 1).then(
+      function () {
+        $location.path('/goods/'+$scope.paramEngine+'/cart');
+      }
+    );
+  }
+
+}).filter('parseImage', function() {
+  return function(item) {
+    try {
+      return JSON.parse(item).image;
+    } catch (e) {
+    }
+  }
+}).filter('parseDescription', function() {
+  return function(item) {
+    try {
+      return JSON.parse(item).description;
+    } catch (e) {
+    }
+  }
+})
 })();
