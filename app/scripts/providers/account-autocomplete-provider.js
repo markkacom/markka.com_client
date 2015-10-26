149a3b75d18bb69f7221c4bb9d95cb2c8a35ee62
Dirk <incentivetoken@gmail.com>
2015-10-26 13:40:06 +0100

    Add support for NXT

--- app/scripts/providers/account-autocomplete-provider.js
+++ app/scripts/providers/account-autocomplete-provider.js
@@ -1,7 +1,7 @@
 (function () {
 'use strict';
 var module = angular.module('fim.base');
-module.factory('AccountAutocompleteProvider', function (nxt, $q, $timeout, $rootScope) {
+module.factory('AccountAutocompleteProvider', function (nxt, $q, $rootScope) {
   function AccountAutocompleteProvider(api) {
     this.api = api;
   }
@@ -16,20 +16,26 @@ module.factory('AccountAutocompleteProvider', function (nxt, $q, $timeout, $root
       }
       this.api.engine.socket().callAPIFunction(args).then(
         function (data) {
-          console.log('data', data);
-          if (data.accounts.length == 1 &&
-              data.accounts[0].identifier == data.accounts[0].accountRS &&
-              data.accounts[0].identifier == query) {
+          var accounts = data.accounts||[];
+          if (accounts.length == 1 &&
+              accounts[0].identifier == accounts[0].accountRS &&
+              accounts[0].identifier == query) {
             deferred.resolve([]);
           }
           else {
-            deferred.resolve(data.accounts.map(
+            deferred.resolve(accounts.map(
               function (obj) {
                 var v = {
                   label: obj.identifier,
                   id_rs: obj.accountRS
                 };
-                if (v.label != v.id_rs) {
+                if ("FIM-"+v.label==v.id_rs) {
+                  v.label = "FIM-"+v.label;
+                }
+                else if ("NXT-"+v.label==v.id_rs) {
+                  v.label = "NXT-"+v.label;
+                }                
+                else {
                   v.name = v.label;
                 }
                 return v;
