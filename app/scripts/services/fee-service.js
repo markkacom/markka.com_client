ee745bcdc824da137ed9a268e2fef132fcdf9d50
Dirk <incentivetoken@gmail.com>
2016-01-02 17:16:24 +0100

    Fee service for dynamically determining fees (uncompleted)

---
+++ app/scripts/services/fee-service.js
@@ -0,0 +1,14 @@
+(function () {
+'use strict';
+var module = angular.module('fim.base');
+module.factory('FeeService', function ($q, nxt, $rootScope) {
+  return {
+    ConstantFee: function (fn) {
+
+    },
+    SizeBasedFee: function (fn) {
+
+    }
+  }
+});
+})();
