1d3acb408a6a1a9cc06608000ec17f09af5a27dc
Dirk <incentivetoken@gmail.com>
2016-01-03 13:45:39 +0100

    Wrap in anonymous function closure, add debounce method

--- app/scripts/lib/init_lompsa.js
+++ app/scripts/lib/init_lompsa.js
@@ -1,23 +1,53 @@
-try {
-  isNodeJS = typeof require == 'function' && require('child_process');
-} catch (e) {
-  isNodeJS = false;
-}
-DEBUG = false;
-
-if (IS_TEST_NET === false) {
-  if (("www.mofowallet.com" == window.location.host || 
-       "mofowallet.com" == window.location.host) && (window.location.protocol != "https:")) {
-    window.location.protocol = "https";
+(function () {
+  try {
+    window.isNodeJS = typeof require == 'function' && require('child_process');
+  } catch (e) {
+    window.isNodeJS = false;
   }
-  else if ("fimkrypto.github.io" == window.location.host) {
-    window.location = "https://www.mofowallet.com/launch.html" + window.location.hash;
-  }
-}
+  window.DEBUG = false;
 
-var entityMap = {"&":"&amp;","<":"&lt;",">":"&gt;",'"':'&quot;',"'":'&#39;',"/":'&#x2F;',"\n":'<br>'};
-function escapeHtml(string) {
-  return String(string).replace(/[&<>"'\/]|[\n]/g, function (s) {
-    return entityMap[s];
-  });
-}
+  var entityMap = {"&":"&amp;","<":"&lt;",">":"&gt;",'"':'&quot;',"'":'&#39;',"/":'&#x2F;',"\n":'<br>'};
+  window.escapeHtml = function (string) {
+    return String(string).replace(/[&<>"'\/]|[\n]/g, function (s) {
+      return entityMap[s];
+    });
+  }
+
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing.
+  //
+  // Source: underscore.js
+  window.debounce = function (func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  Function.prototype.debounce = function (threshold, execAsap) {
+    var func = this, timeout;
+    return function debounced () {
+      var obj = this, args = arguments;
+      function delayed () {
+        if (!execAsap)
+          func.apply(obj, args);
+        timeout = null; 
+      };
+      if (timeout)
+        clearTimeout(timeout);
+      else if (execAsap)
+        func.apply(obj, args);
+      timeout = setTimeout(delayed, threshold || 100); 
+    };
+  }
+})();
